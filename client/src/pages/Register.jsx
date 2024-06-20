2216ec45e5e349526aeb34e158827171d8d1da9f
subha20032007 <meunknown466@gmail.com>
2024-06-20 23:57:25 +0530

    Ad

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
     //    axios.post(``)
     toast.success('Register Successfully')
     }
-    //1234
+    //12345
   return (
     <Layout>
         <div className='register'>
