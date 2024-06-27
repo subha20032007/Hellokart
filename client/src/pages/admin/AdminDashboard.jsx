0c80073ae4b03d1030c1e34d2c6c291e6bd20274
subha20032007 <meunknown466@gmail.com>
2024-06-27 23:49:44 +0530

    admin-dashboard

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Layout from '../../componets/Layout'
+import AdminMenu from '../../componets/AdminMenu'
 
 const AdminDashboard = () => {
   return (
-    <Layout>AdminDashboard</Layout>
+    <Layout>
+        <div>
+            <AdminMenu/>
+        </div>
+    </Layout>
   )
 }
 
